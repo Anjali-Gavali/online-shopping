acdf26afe427eb271d726a641eaa2a999ae1dd07
Anjali Gavali <gavalia14@gmail.com>
2019-11-17 16:51:17 +0530

    Completed part 06 - 3

--- shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/UserTestCase.java
+++ shoppingbackend/src/test/java/net/kzn/shoppingbackend/test/UserTestCase.java
@@ -29,7 +29,7 @@ public class UserTestCase {
 		userDAO = (UserDAO) context.getBean("userDAO");
 	}
 	
-	@Test
+/*	@Test
 	public void testAdd() {
 		
 		
@@ -58,7 +58,6 @@ public class UserTestCase {
 		
 		// add the address
 		assertEquals("Failed to add address!",true, userDAO.addAddress(address));
-
 		
 		if(user.getRole().equals("USER")) {
 			
@@ -66,13 +65,11 @@ public class UserTestCase {
 			cart = new Cart();
 			
 			cart.setUser(user);
-
 			// add the cart
 			assertEquals("Failed to add cart!",true, userDAO.addCart(cart));
 			
 			
 			// add a shipping address for this user
-
 			address = new Address();
 			address.setAddressLineOne("201/B Jadoo Society, Kishan Kanhaiya Nagar");
 			address.setAddressLineTwo("Near Kudrat Store");
@@ -88,18 +85,55 @@ public class UserTestCase {
 			
 			// add the shipping address
 			assertEquals("Failed to add shipping address!",true, userDAO.addAddress(address));
-			
-			
-			
 		}
+	}
+	*/
+
+/*	@Test
+	public void testAdd() {
+		user = new User() ;
+		user.setFirstName("Hrithik");
+		user.setLastName("Roshan");
+		user.setEmail("hr@gmail.com");
+		user.setContactNumber("1234512345");
+		user.setRole("USER");
+		user.setPassword("123456");
 		
+		if(user.getRole().equals("USER")) {			
+			// create a cart for this user
+			cart = new Cart();
+			
+			cart.setUser(user);
+			
+			// attach cart with the user
+			user.setCart(cart);
+		}
+		// add the user
+		assertEquals("Failed to add user!",true, userDAO.addUser(user));
+	}
 		
+	*/
+	
+	@Test
+	public void testUpdateCart() {
 		
+		// fetch the user by its email
+		user = userDAO.getByEmail("hr@gmail.com");
 		
+		// get the cart of the user
+		cart = user.getCart();
 		
+		cart.setGrandTotal(5555);
+		
+		cart.setCartLines(2);
+		
+		assertEquals("Failed to update the cart!", true, userDAO.updateCart(cart));
 		
 		
 	}
+	
+	
+	
 	
 	
 }
